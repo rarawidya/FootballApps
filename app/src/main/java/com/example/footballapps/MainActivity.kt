68f6c6db17921ada60ace077d6c0f3e021738730
Rara Widya <rara12148@gmail.com>
2019-10-21 20:05:37 +0700

    Recycle view list

--- app/src/main/java/com/example/footballapps/MainActivity.kt
+++ app/src/main/java/com/example/footballapps/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.footballapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.footballapps.adapter.ListClubAdapter
+import com.example.footballapps.model.Club
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvClub: RecyclerView
+    private var list: ArrayList<Club> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvClub = findViewById(R.id.rvClub)
+        rvClub.setHasFixedSize(true)
+
+        list.addAll(ClubData.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvClub.layoutManager = LinearLayoutManager(this)
+
+        val listClubAdapter = ListClubAdapter(list)
+        rvClub.adapter = listClubAdapter
     }
 }
